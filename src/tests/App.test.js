74343c297d09f30c2f5ebfbe76dafbb22430d0e0
GedionT <teshomegedion@gmail.com>
2021-11-25 12:09:39 +0300

    minor test fix

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from "@testing-library/react";
 import App from "../App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("true is truthy and false is falsy", () => {
+  test("true is truthy", () => {
+    expect(true).toBe(true);
+  });
+
+  test("false is falsy", () => {
+    expect(false).toBe(false);
+  });
+});
+
+describe("render testing dummy tests", () => {
+  test("failing test to check renders learn react link", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("under construction text rendered", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/under construction/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
