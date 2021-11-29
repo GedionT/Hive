78a10b3ef773861bbb6b4c83d39ce62210bc4f63
GedionT <teshomegedion@gmail.com>
2021-11-29 04:19:50 +0300

    icon init🍦

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from "react";
-import "./styles/index.css";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import App from "./App";
+import React from 'react';
+import './styles/index.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
 
-import reportWebVitals from "./utils/reportWebVitals";
-import * as serviceWorkerRegistration from "./utils/serviceWorkerRegistration";
-import ReactGA from "react-ga";
+import reportWebVitals from './utils/reportWebVitals';
+import * as serviceWorkerRegistration from './utils/serviceWorkerRegistration';
+import ReactGA from 'react-ga';
+import { initializeIcons } from '@fluentui/react/lib/Icons';
 
-ReactGA.initialize("UA-xxxxxxxxxx-x");
+initializeIcons(/* optional base url */);
+
+ReactGA.initialize('UA-xxxxxxxxxx-x');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
@@ -17,16 +20,16 @@ ReactDOM.render(
       <App />
     </Router>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 serviceWorkerRegistration.register();
 
 function sendToAnalytics({ id, name, value }) {
   ReactGA.event({
-    category: "Web Vitals",
+    category: 'Web Vitals',
     action: name,
-    value: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
+    value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
     label: id, // id unique to current page load
     nonInteraction: true, // avoids affecting bounce rate
   });
