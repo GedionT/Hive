bf413036f7a61b70b9cbb003754e3dd44b4610c0
GedionT <teshomegedion@gmail.com>
2021-11-29 04:42:48 +0300

    minor 🔺

--- src/stories/Card.stories.js
+++ src/stories/Card.stories.js
@@ -3,13 +3,32 @@ import DataCard from '../components/dataCard.jsx';
 export default {
   title: 'DataCard',
   component: DataCard,
-  argTypes: { handleClick: { action: 'clicked' } }, 
+  argTypes: { handleClick: { action: 'clicked' } },
 };
 
 const Template = (args) => <DataCard {...args} />;
 
-export const Card = Template.bind({});
-Card.args = {
-  label: 'Card',
-  size: 'medium',
+export const Cards = Template.bind({});
+Cards.args = {
+  people: [
+    {
+      name: 'Kidus Teshome',
+      profileImageSrc: 'https://avatars.githubusercontent.com/u/26666155?v=4',
+    },
+    {
+      name: 'Savannah Johnson',
+      profileImageSrc: 'https://avatars.githubusercontent.com/u/26666155?v=4',
+    },
+  ],
+  oneNoteIconProps: {
+    iconName: 'OneNoteLogo',
+    styles: {
+      root: {
+        color: '#00000',
+        fontSize: '120px',
+        width: '120px',
+        height: '120px',
+      },
+    },
+  },
 };
