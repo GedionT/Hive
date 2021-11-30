4220c00047d1406497926045fc96b44176d5948e
GedionT <teshomegedion@gmail.com>
2021-11-30 16:45:15 +0300

    converted createElement -> jsx

--- src/components/dataCard.jsx
+++ src/components/dataCard.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import {
   DocumentCard,
   DocumentCardActivity,
@@ -7,7 +7,6 @@ import {
   DocumentCardImage,
 } from '@fluentui/react/lib/DocumentCard';
 import PropTypes from 'prop-types';
-
 import { ImageFit } from '@fluentui/react/lib/Image';
 
 const people = [
@@ -24,9 +23,12 @@ const people = [
     name: 'Fasika Zewdie',
     profileImageSrc: 'https://avatars.githubusercontent.com/u/26666155?v=4',
   },
-  { name: 'Getachew Muhabaw', profileImageSrc: '', initials: 'GM' },
+  {
+    name: 'Getachew Muhabaw',
+    profileImageSrc: '',
+    initials: 'GM',
+  },
 ];
-
 const oneNoteIconProps = {
   iconName: 'OneNoteLogo',
   styles: {
@@ -47,69 +49,60 @@ export default function DataCard() {
       width: 320,
     },
   };
-  return React.createElement(
-    'div',
-    null,
-    React.createElement(
-      DocumentCard,
-      {
-        'aria-label':
+  return (
+    <div>
+      <DocumentCard
+        aria-label={
           'Document Card with image. How to make a good design. ' +
-          'Last modified by Annie Lindqvist and 2 others in March 13, 2018.',
-        styles: cardStyles,
-        onClickHref: 'http://github.com/gediont',
-      },
-      React.createElement(DocumentCardImage, {
-        height: 150,
-        imageFit: ImageFit.cover,
-        imageSrc:
-          'https://www.saintjohnscancer.org/blog/wp-content/uploads/sites/11/2020/09/iStock-1175329868-1024x630-1.jpg',
-      }),
-      React.createElement(
-        DocumentCardDetails,
-        null,
-        React.createElement(DocumentCardTitle, {
-          title: 'Campaign for Breast Cancer Awareness',
-          shouldTruncate: true,
-        })
-      ),
-      React.createElement(DocumentCardActivity, {
-        activity: 'Modified March 13, 2021',
-        people: people.slice(0, 3),
-      })
-    ),
-    React.createElement(
-      DocumentCard,
-      {
-        'aria-label':
+          'Last modified by Annie Lindqvist and 2 others in March 13, 2018.'
+        }
+        styles={cardStyles}
+        onClickHref="http://github.com/gediont"
+      >
+        <DocumentCardImage
+          height={150}
+          imageFit={ImageFit.cover}
+          imageSrc="https://www.saintjohnscancer.org/blog/wp-content/uploads/sites/11/2020/09/iStock-1175329868-1024x630-1.jpg"
+        />
+        <DocumentCardDetails>
+          <DocumentCardTitle
+            title="Campaign for Breast Cancer Awareness"
+            shouldTruncate={true}
+          />
+        </DocumentCardDetails>
+        <DocumentCardActivity
+          activity="Modified March 13, 2021"
+          people={people.slice(0, 3)}
+        />
+      </DocumentCard>
+      <DocumentCard
+        aria-label={
           'Document Card with icon. How to make a good design. ' +
-          'Last modified by Christian Bergqvist in January 1, 2019.',
-        styles: cardStyles,
-        onClickHref: 'http://github.com/gediont',
-      },
-      React.createElement(DocumentCardImage, {
-        height: 150,
-        imageFit: ImageFit.cover,
-        imageSrc:
-          'https://media.istockphoto.com/photos/pensive-school-boy-thinking-about-homework-project-writing-essay-picture-id962478086?k=20&m=962478086&s=612x612&w=0&h=PjHlmSAYIdBmhSNha5KF8BCt8KxntIRJL-VeR6NtaGI=',
-        iconProps: oneNoteIconProps,
-      }),
-      React.createElement(
-        DocumentCardDetails,
-        null,
-        React.createElement(DocumentCardTitle, {
-          title: 'Teach an ADHD affected student a skill',
-          shouldTruncate: true,
-        })
-      ),
-      React.createElement(DocumentCardActivity, {
-        activity: 'Modified January 1, 2022',
-        people: [people[3]],
-      })
-    )
+          'Last modified by Christian Bergqvist in January 1, 2019.'
+        }
+        styles={cardStyles}
+        onClickHref="http://github.com/gediont"
+      >
+        <DocumentCardImage
+          height={150}
+          imageFit={ImageFit.cover}
+          imageSrc="https://media.istockphoto.com/photos/pensive-school-boy-thinking-about-homework-project-writing-essay-picture-id962478086?k=20&m=962478086&s=612x612&w=0&h=PjHlmSAYIdBmhSNha5KF8BCt8KxntIRJL-VeR6NtaGI="
+          iconProps={oneNoteIconProps}
+        />
+        <DocumentCardDetails>
+          <DocumentCardTitle
+            title="Teach an ADHD affected student a skill"
+            shouldTruncate={true}
+          />
+        </DocumentCardDetails>
+        <DocumentCardActivity
+          activity="Modified January 1, 2022"
+          people={[people[3]]}
+        />
+      </DocumentCard>
+    </div>
   );
 }
-
 DataCard.propTypes = {
   people: PropTypes.arrayOf(people),
   oneNoteIconProps: PropTypes.object,
--- src/components/weekPicker.jsx
+++ src/components/weekPicker.jsx
@@ -1,18 +1,16 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback } from 'react';
 import {
   Calendar,
   DateRangeType,
   DayOfWeek,
   defaultCalendarStrings,
-} from "@fluentui/react";
-
+} from '@fluentui/react';
 const workWeekDays = [
   DayOfWeek.Tuesday,
   DayOfWeek.Saturday,
   DayOfWeek.Wednesday,
   DayOfWeek.Friday,
 ];
-
 export default function WeekPicker() {
   const [selectedDateRange, setSelectedDateRange] = useState();
   const [selectedDate, setSelectedDate] = useState();
@@ -20,40 +18,38 @@ export default function WeekPicker() {
     setSelectedDate(date);
     setSelectedDateRange(dateRangeArray);
   }, []);
-
-  let dateRangeString = "Not set";
+  let dateRangeString = 'Not set';
 
   if (selectedDateRange) {
     const rangeStart = selectedDateRange[0];
     const rangeEnd = selectedDateRange[selectedDateRange.length - 1];
-
     dateRangeString =
-      rangeStart.toLocaleDateString() + "-" + rangeEnd.toLocaleDateString();
-  }
+      rangeStart.toLocaleDateString() + '-' + rangeEnd.toLocaleDateString();
+  } // covert explicit methods to abstraction later
 
-  // covert explicit methods to abstraction later
-  return React.createElement(
-    "div",
-    { style: { height: "360px" } },
-    React.createElement(
-      "div",
-      null,
-      "Selected date: ",
-      (selectedDate === null || selectedDate === void 0
-        ? void 0
-        : selectedDate.toLocaleString()) || "Not set"
-    ),
-    React.createElement("div", null, "Selected range: ", dateRangeString),
-    React.createElement(Calendar, {
-      dateRangeType: DateRangeType.WorkWeek,
-      workWeekDays: workWeekDays,
-      firstDayOfWeek: DayOfWeek.Monday,
-      highlightSelectedMonth: true,
-      showGoToToday: true,
-      onSelectDate: onSelectDate,
-      value: selectedDate,
-      // Calendar uses English strings by default. For localized apps, you must override this prop.
-      strings: defaultCalendarStrings,
-    })
+  return (
+    <div
+      style={{
+        height: '360px',
+      }}
+    >
+      <div>
+        Selected date:{' '}
+        {(selectedDate === null || selectedDate === void 0
+          ? void 0
+          : selectedDate.toLocaleString()) || 'Not set'}
+      </div>
+      <div>Selected range: {dateRangeString}</div>
+      <Calendar
+        dateRangeType={DateRangeType.WorkWeek}
+        workWeekDays={workWeekDays}
+        firstDayOfWeek={DayOfWeek.Monday}
+        highlightSelectedMonth={true}
+        showGoToToday={true}
+        onSelectDate={onSelectDate}
+        value={selectedDate}
+        strings={defaultCalendarStrings}
+      />
+    </div>
   );
 }
