d15c8bf3042deed14c38d0cb96aae88a81b836bc
GedionT <teshomegedion@gmail.com>
2021-12-01 06:45:52 +0300

    extracted props

--- src/components/dataCard.jsx
+++ src/components/dataCard.jsx
@@ -9,37 +9,9 @@ import {
 import PropTypes from 'prop-types';
 import { ImageFit } from '@fluentui/react/lib/Image';
 
-const people = [
-  {
-    name: 'Gedion Teshome',
-    profileImageSrc: 'https://avatars.githubusercontent.com/u/26666155?v=4',
-  },
-  {
-    name: 'Henok Fisseha',
-    profileImageSrc: 'https://avatars.githubusercontent.com/u/26666155?v=4',
-    initials: 'HFT',
-  },
-  {
-    name: 'Fasika Zewdie',
-    profileImageSrc: 'https://avatars.githubusercontent.com/u/26666155?v=4',
-  },
-  {
-    name: 'Getachew Muhabaw',
-    profileImageSrc: '',
-    initials: 'GM',
-  },
-];
-const oneNoteIconProps = {
-  iconName: 'OneNoteLogo',
-  styles: {
-    root: {
-      color: '#813a7c',
-      fontSize: '120px',
-      width: '120px',
-      height: '120px',
-    },
-  },
-};
+// mock data import props
+import { openData } from '../services/mockData';
+
 export default function DataCard() {
   const cardStyles = {
     root: {
@@ -49,61 +21,31 @@ export default function DataCard() {
       width: 320,
     },
   };
+
   return (
     <div>
       <DocumentCard
-        aria-label={
-          'Document Card with image. How to make a good design. ' +
-          'Last modified by Annie Lindqvist and 2 others in March 13, 2018.'
-        }
+        aria-label={'Open role Card with image. '}
         styles={cardStyles}
-        onClickHref="http://github.com/gediont"
+        onClickHref="/"
       >
         <DocumentCardImage
           height={150}
           imageFit={ImageFit.cover}
-          imageSrc="https://www.saintjohnscancer.org/blog/wp-content/uploads/sites/11/2020/09/iStock-1175329868-1024x630-1.jpg"
+          imageSrc={openData.imgSrc}
         />
         <DocumentCardDetails>
-          <DocumentCardTitle
-            title="Campaign for Breast Cancer Awareness"
-            shouldTruncate={true}
-          />
+          <DocumentCardTitle title={openData.title} shouldTruncate={true} />
         </DocumentCardDetails>
         <DocumentCardActivity
-          activity="Modified March 13, 2021"
-          people={people.slice(0, 3)}
-        />
-      </DocumentCard>
-      <DocumentCard
-        aria-label={
-          'Document Card with icon. How to make a good design. ' +
-          'Last modified by Christian Bergqvist in January 1, 2019.'
-        }
-        styles={cardStyles}
-        onClickHref="http://github.com/gediont"
-      >
-        <DocumentCardImage
-          height={150}
-          imageFit={ImageFit.cover}
-          imageSrc="https://media.istockphoto.com/photos/pensive-school-boy-thinking-about-homework-project-writing-essay-picture-id962478086?k=20&m=962478086&s=612x612&w=0&h=PjHlmSAYIdBmhSNha5KF8BCt8KxntIRJL-VeR6NtaGI="
-          iconProps={oneNoteIconProps}
-        />
-        <DocumentCardDetails>
-          <DocumentCardTitle
-            title="Teach an ADHD affected student a skill"
-            shouldTruncate={true}
-          />
-        </DocumentCardDetails>
-        <DocumentCardActivity
-          activity="Modified January 1, 2022"
-          people={[people[3]]}
+          activity={openData.activity}
+          people={openData.people.slice(0, 3)}
+          // {[people[3]]} another option
         />
       </DocumentCard>
     </div>
   );
 }
 DataCard.propTypes = {
-  people: PropTypes.arrayOf(people),
-  oneNoteIconProps: PropTypes.object,
+  openData: PropTypes.object,
 };
---
+++ src/services/mockData.js
@@ -0,0 +1,25 @@
+export const openData = {
+  title: 'Campaign for Breast Cancer Awareness',
+  imgSrc:
+    'https://www.saintjohnscancer.org/blog/wp-content/uploads/sites/11/2020/09/iStock-1175329868-1024x630-1.jpg',
+  activity: 'Modified March 13, 2021',
+  people: [
+    {
+      name: 'Gedion Teshome',
+      profileImageSrc: 'https://avatars.githubusercontent.com/u/26666155?v=4',
+    },
+    {
+      name: 'Henok Fisseha',
+      initials: 'HF',
+    },
+    {
+      name: 'Fasika Zewdie',
+      profileImageSrc: 'https://avatars.githubusercontent.com/u/26666155?v=4',
+    },
+    {
+      name: 'Getachew Muhabaw',
+      profileImageSrc: '',
+      initials: 'GM',
+    },
+  ],
+};
